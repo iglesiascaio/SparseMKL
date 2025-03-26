4764848d02478482ada25aa2ced8ba1641f1b444
Caio Iglesias <caiopiglesias@gmail.com>
2025-03-26 10:17:50 -0400

    update memory limit for lowerbound formulations

--- scripts/run_lowerbound_formulations.sh
+++ scripts/run_lowerbound_formulations.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-#SBATCH --cpus-per-task=4            # Number of CPUs per task
-#SBATCH --mem=180G                    # Total memory per node
+#SBATCH --cpus-per-task=48            # Number of CPUs per task
+#SBATCH --mem=187G                    # Total memory per node
 
 # Loading the required module
 module load anaconda/Python-ML-2025a
 module load julia/1.1.10.1
+
+ulimit -s unlimited
 
 # Run the script
 julia run_lowerbound_formulations.jl
